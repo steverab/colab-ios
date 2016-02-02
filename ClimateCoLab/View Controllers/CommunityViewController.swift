7e9614b9a7429be1dad93c095e539ded6725d726
Stephan Rabanser <steverab93@gmail.com>
2016-02-02 00:00:12 -0500

    Add general navigation

--- ClimateCoLab/View Controllers/CommunityViewController.swift
+++ ClimateCoLab/View Controllers/CommunityViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CommunityViewController.swift
 //  ClimateCoLab
 //
 //  Created by Stephan Rabanser on 01/02/16.
@@ -8,18 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class CommunityViewController: UIViewController {
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
 }
-
---
+++ ClimateCoLab/View Controllers/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  ClimateCoLab
+//
+//  Created by Stephan Rabanser on 01/02/16.
+//  Copyright © 2016 MIT. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+}
